fcde9bae143fdc392afcf3c465a79af19635d5dc
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2022-01-11 12:36:43 -0300

    Projeto focado em autorizaçao e autenticacao

---
+++ services/api.ts
@@ -0,0 +1,78 @@
+import axios, { AxiosError } from 'axios';
+import Router from 'next/router';
+import {destroyCookie, parseCookies, setCookie} from 'nookies';
+import { signOut } from '../contexts/AuthContext';
+
+let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestQueue = [];
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3333',
+  headers: {
+    Authorization: `Bearer ${cookies['projectauth.token']}`
+  }
+});
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: AxiosError) => {
+  if(error.response.status === 401) {
+    if(error.response.data?.code === 'token.expired') {
+      cookies = parseCookies();
+
+      const {'projectauth.refreshToken': refreshToken} = cookies;
+
+      const originalConfig = error.config;
+
+      if(!isRefreshing) {
+        isRefreshing = true;
+
+        api.post('/refresh', {
+          refreshToken,
+        }).then(response => {
+          const { token } = response.data;
+  
+          setCookie(undefined, 'projectauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/',
+          });
+    
+          setCookie(undefined, 'projectauth.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/',
+          });
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
+          
+          failedRequestQueue.forEach(request => request.onSuccess(token));
+          failedRequestQueue = [];
+        }).catch(err => {
+          failedRequestQueue.forEach(request => request.onFailure(err));
+          failedRequestQueue = [];
+
+        }).finally(() => {
+          isRefreshing = false;
+        });
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`;
+
+            resolve(api(originalConfig));
+          },
+          onFailure: (err: AxiosError) => {
+            reject(err);
+          }
+        })
+      })
+
+    } else {
+      signOut();
+    }
+  }
+
+  return Promise.reject(error);
+});
